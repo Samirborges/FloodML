fbf1ccc47cef52acd8356c3f4ebb9e64c00381d7
josealvesdantasjunior@gmail.com <167125176+josealvesdantasjunior@users.noreply.github.com>
2026-05-21 17:58:27 -0300

    criando estrutura de pastas solicitada

--- main.py
+++
@@ -1,8 +0,0 @@
-from src.models.baseline_logistic import LogisticModel
-from src.preprocessing.split_data import SplitData
-
-X_train, X_test, y_train, y_test = SplitData.split()
-
-model = LogisticModel.train(X_train, y_train)
-
-print(model)
