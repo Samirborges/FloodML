8b87df05d83e2d83bbbd9fe433a5e761399fa723
Samirborges <alessandro.samir@souunit.com.br>
2026-05-22 15:12:28 -0300

    feat(evaluete): evaluete added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from src.models.baseline_logistic import LogisticModel
 from src.preprocessing.split_data import SplitData
+from src.models.evaluete import EvaluateModel
 
 X_train, X_test, y_train, y_test = SplitData.split()
 
 model = LogisticModel.train(X_train, y_train)
 
 print(model)
+print(EvaluateModel.evaluate(model, X_test, y_test))
